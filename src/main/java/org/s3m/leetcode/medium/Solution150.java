9078dbf22c44c0767f7c207e9afc744059790e51
vasyl <vasjasynenko@gmail.com>
2026-02-13 12:08:00 +0200

    LC-150 : Container With Most Water

---
+++ src/main/java/org/s3m/leetcode/medium/Solution150.java
@@ -0,0 +1,64 @@
+package org.s3m.leetcode.medium;
+
+
+import java.util.Arrays;
+
+/**
+ * Container With Most Water
+ * <p>
+ * You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+ * Find two lines that together with the x-axis form a container, such that the container contains the most water.
+ * Return the maximum amount of water a container can store.
+ * Notice that you may not slant the container.
+ * <p>
+ * Example 1:
+ * Input: height = [1,8,6,2,5,4,8,3,7]
+ * Output: 49
+ * Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+ * Example 2:
+ * Input: height = [1,1]
+ * Output: 1
+ */
+public class Solution150 {
+
+    public int maxArea(int[] height) {
+        int l = 0;
+        int r = height.length - 1;
+        int max = 0;
+        do {
+            int lVal = height[l];
+            int rVal = height[r];
+            int actHeight = Math.min(lVal, rVal);
+            int distance = r-l;
+            int curVal = distance * actHeight;
+            if(max < curVal) {
+                max = curVal;
+            }
+            //todo: improvement  if next val is smaller we can move pointer faster
+            if(lVal > rVal) {
+                r--;
+            } else {
+                l++;
+            }
+
+        } while(l < r);
+
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        // Example 1
+        int[] height = {1,8,6,2,5,4,8,3,7};
+        System.out.println("Input:    " + Arrays.toString(height));
+        System.out.println("Output:   " + new Solution150().maxArea(height));
+        System.out.println("Expected: 49");
+
+        // Example 2
+        height = new int[]{1,1};
+        System.out.println("Input:    " + Arrays.toString(height));
+        System.out.println("Output:   " + new Solution150().maxArea(height));
+        System.out.println("Expected: 1");
+
+    }
+}
