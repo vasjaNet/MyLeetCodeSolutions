241d172c5d77ec0a9d3b5e7d49795d40c122f2c9
vasyl <vasjasynenko@gmail.com>
2026-02-18 13:27:15 +0200

    LC-36 : Valid Sudoku

---
+++ src/main/java/org/s3m/leetcode/medium/Solution36.java
@@ -0,0 +1,110 @@
+package org.s3m.leetcode.medium;
+
+
+import java.util.*;
+
+/**
+ * Valid Sudoku
+ * <p>
+ * Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+ * Each row must contain the digits 1-9 without repetition.
+ * Each column must contain the digits 1-9 without repetition.
+ * Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+ * Note:
+ * A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+ * Only the filled cells need to be validated according to the mentioned rules.
+ * <p>
+ * Example 1:
+ * Input: board =
+ * [["5","3",".",".","7",".",".",".","."]
+ * ,["6",".",".","1","9","5",".",".","."]
+ * ,[".","9","8",".",".",".",".","6","."]
+ * ,["8",".",".",".","6",".",".",".","3"]
+ * ,["4",".",".","8",".","3",".",".","1"]
+ * ,["7",".",".",".","2",".",".",".","6"]
+ * ,[".","6",".",".",".",".","2","8","."]
+ * ,[".",".",".","4","1","9",".",".","5"]
+ * ,[".",".",".",".","8",".",".","7","9"]]
+ * Output: true
+ * Example 2:
+ * Input: board =
+ * [["8","3",".",".","7",".",".",".","."]
+ * ,["6",".",".","1","9","5",".",".","."]
+ * ,[".","9","8",".",".",".",".","6","."]
+ * ,["8",".",".",".","6",".",".",".","3"]
+ * ,["4",".",".","8",".","3",".",".","1"]
+ * ,["7",".",".",".","2",".",".",".","6"]
+ * ,[".","6",".",".",".",".","2","8","."]
+ * ,[".",".",".","4","1","9",".",".","5"]
+ * ,[".",".",".",".","8",".",".","7","9"]]
+ * Output: false
+ * Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+ */
+public class Solution36 {
+
+    public boolean isValidSudoku(char[][] board) {
+        //todo: optimize try Map -> int[]
+        Map<Integer, Set<Character>> rows = new HashMap<>();
+        Map<Integer, Set<Character>> columns = new HashMap<>();
+        Map<Integer, Set<Character>> boxes = new HashMap<>();
+        for(int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char c = board[i][j];
+                if (c == '.') continue;
+                if (rows.computeIfAbsent(i, k -> new HashSet<>()).contains(c)) {
+                    return false;
+                } else {
+                    rows.get(i).add(c);
+                }
+                if (columns.computeIfAbsent(j, k -> new HashSet<>()).contains(c)) {
+                    return false;
+                } else {
+                    columns.get(j).add(c);
+                }
+                int boxIndex = (i/3)*3 + j/3;
+                if (boxes.computeIfAbsent(boxIndex, k -> new HashSet<>()).contains(c)) {
+                    return false;
+                } else {
+                    boxes.get(boxIndex).add(c);
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        // Example 1
+        char[][] board = {
+                {'5','3','.','.','7','.','.','.','.'},
+                {'6','.','.','1','9','5','.','.','.'},
+                {'.','9','8','.','.','.','.','6','.'},
+                {'8','.','.','.','6','.','.','.','3'},
+                {'4','.','.','8','.','3','.','.','1'},
+                {'7','.','.','.','2','.','.','.','6'},
+                {'.','6','.','.','.','.','2','8','.'},
+                {'.','.','.','4','1','9','.','.','5'},
+                {'.','.','.','.','8','.','.','7','9'}
+        };
+        System.out.println("Input:    " + Arrays.deepToString(board));
+        System.out.println("Output:   " + new Solution36().isValidSudoku(board));
+        System.out.println("Expected: true");
+
+        // Example 2
+        board = new char[][] {
+                {'8','3','.','.','7','.','.','.','.'},
+                {'6','.','.','1','9','5','.','.','.'},
+                {'.','9','8','.','.','.','.','6','.'},
+                {'8','.','.','.','6','.','.','.','3'},
+                {'4','.','.','8','.','3','.','.','1'},
+                {'7','.','.','.','2','.','.','.','6'},
+                {'.','6','.','.','.','.','2','8','.'},
+                {'.','.','.','4','1','9','.','.','5'},
+                {'.','.','.','.','8','.','.','7','9'}
+        };
+        System.out.println("Input:    " + Arrays.deepToString(board));
+        System.out.println("Output:   " + new Solution36().isValidSudoku(board));
+        System.out.println("Expected: false");
+
+    }
+}
